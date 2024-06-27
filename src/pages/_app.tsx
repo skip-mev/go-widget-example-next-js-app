4546db7daa0ee2139747ce697fca017ca42a8c15
Jeremy Liu <31809888+NotJeremyLiu@users.noreply.github.com>
2024-06-27 06:14:37 -0700

    integrate widget

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { SwapWidgetProvider } from '@skip-go/widget';
+import '@skip-go/widget/style.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SwapWidgetProvider>
+      <Component {...pageProps} />
+    </SwapWidgetProvider>
+);
 }
